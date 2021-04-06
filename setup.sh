bf51a88e06ca335219660337f7a9b04185dacc8b
Justin <justinfox1206@gmail.com>
2021-04-06 14:04:20 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo apt install zsh
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 cp ./.zshrc ~/.zshrc
-source ~/.zshrc
